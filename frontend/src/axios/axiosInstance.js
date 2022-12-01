d2fe0d1c16707671ea6d37a25c75e96e96db372c
varun reddy <71369489+varunreddy1268@users.noreply.github.com>
2022-12-01 14:04:38 -0800

    a

--- frontend/src/axios/axiosInstance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
-});
-
-axiosInstance.defaults.headers.common["Authorization"] = localStorage.getItem('token');
-
-export default axiosInstance;
